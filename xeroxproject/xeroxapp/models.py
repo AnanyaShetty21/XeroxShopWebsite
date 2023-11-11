2a618250ea5d382946b22e285adfa01113ee1e14
Ananya Shetty <ananyanisarga21@gmail.com>
2023-11-11 23:14:59 +0530

    created a superuser

--- xeroxproject/xeroxapp/models.py
+++ xeroxproject/xeroxapp/models.py
@@ -1,5 +1,5 @@
 #IF THIS PAGE IS MODIFIED, REMEMBER TO EXCECUTE THE FOLLOWING COMMANDS IN PYTHON SHELL
-#python manage.py makemigrations
+#python manage.py makemigrations xeroxapp
 #python manage.py migrate
 
 from django.db import models
@@ -12,9 +12,21 @@ class ownerPDFlist(models.Model):
     def __str__(self):
         return self.name
     
+    #Meta data for ownerPDFlist
+    #Else the display name in the admin dashboard will be "Owner pd flists"
+    class Meta:
+        verbose_name="ownerPDFlist"
+        verbose_name_plural="ownerPDFlist"
+    
 class PDF(models.Model):
     ownerPDFlist=models.ForeignKey(ownerPDFlist, on_delete=models.CASCADE) #use 'o.pdf_set.all()' to access all the pdfs present
     text=models.CharField(max_length=300)
 
     def __str__(self):
         return self.text
+    
+    #Meta data for PDF
+    #Else the display name in the admin dashboard will be "Pdfs"
+    class Meta:
+        verbose_name="PDF"
+        verbose_name_plural="PDFs"
--- xeroxproject/xeroxapp/views.py
+++ xeroxproject/xeroxapp/views.py
@@ -8,15 +8,17 @@ from .models import ownerPDFlist, PDF
 
 def owner(response):
     return HttpResponse("<h1>Owner's webpage</h1>")
-#def ownerpdf(response):
- #   ls = ownerPDFlist.objects.get(name = "Owner\'s PDFs") #id=1
-  #  pdf = ls.pdf_set.all()
-   # return HttpResponse("<h1>%s</h1><br></br><p>%s</p>"%(ls.name,str(pdf.text)))
+
+
+"""def ownerpdf(response):
+    ls = ownerPDFlist.objects.get(name = "Notes") #id=1
+    pdf = ls.pdf_set.all()
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>"%(ls.name,str(pdf.text)))"""
 
 
 def ownerpdf(response):
     try:
-        ls = ownerPDFlist.objects.get(name="Owner's PDFs")
+        ls = ownerPDFlist.objects.get(name="Notes")
         pdfs = ls.pdf_set.all()
         
         output = f"<h1>{ls.name}</h1>"
@@ -25,8 +27,8 @@ def ownerpdf(response):
             for pdf in pdfs:
                 output += f"<p>{pdf.text}</p>"
         else:
-            output += "<p>No PDFs found.</p>"
+            output += "<p>No notes found.</p>"
         
         return HttpResponse(output)
     except ownerPDFlist.DoesNotExist:
-        return HttpResponse("Owner's PDFs list does not exist.")
+        return HttpResponse("Notes does not exist.")
